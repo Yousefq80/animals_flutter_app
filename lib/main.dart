b19959398d5e3abe01361bf3a437f043b50b847c
Yousefq80 <ysalharby@gmail.com>
2022-11-14 20:07:40 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("animals_flutter_app")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.yellowAccent,
+              child: Image.asset("assets/images/elefent.png",
+                  height: 300, width: 300, fit: BoxFit.contain),
+            ),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text("Name:Elefent"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text("age:3"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text("gender:Male"),
+              )
+            ])
+          ],
+        ));
+  }
+}
